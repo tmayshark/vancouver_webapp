7fdae88530cbde2d30fd30efcdb53491d34dacc9
Tristan Mayshark <tristan.mayshark@opower.com>
2016-02-03 19:34:01 -0500

    NOW WITH MOAR FRONTEND

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require_relative './lib/smarter_mull'
 include Mulligan
 
 get '/param/:ramp/:curve' do
-  @runner = Mulligan::VariableCountSim.new(1000, params[:ramp].to_i,params[:curve].to_i)
-  output = "<%= @runner.results %>"
-  erb output
+  runner = Mulligan::VariableCountSim.new(1000, params[:ramp].to_i,params[:curve].to_i)
+  @stats= runner.results
+  erb :output
+end
+
+get '/mull' do
+  erb :input_form
+end
+
+post '/mull/' do
+  runner = Mulligan::VariableCountSim.new(1000, params[:ramp].to_i, params[:curve].to_i)
+  @stats = runner.results
+  erb :output
 end
