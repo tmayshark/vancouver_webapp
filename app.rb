582374af2cd7c0ff46a0b40226005e68e5ed6ccd
Tristan Mayshark <tristan.mayshark@opower.com>
2016-02-04 00:02:25 -0500

    add root path

--- app.rb
+++ app.rb
@@ -2,10 +2,8 @@ require 'sinatra'
 require_relative './lib/smarter_mull'
 include Mulligan
 
-get '/param/:ramp/:curve' do
-  runner = Mulligan::VariableCountSim.new(1000, params[:ramp].to_i,params[:curve].to_i)
-  @stats= runner.results
-  erb :output
+get '/' do
+  erb: :input_form
 end
 
 get '/mull' do
