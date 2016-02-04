0ece8598a80c3450cf069ba3a2143f4eb982b79a
Tristan Mayshark <tristan.mayshark@opower.com>
2016-02-03 23:37:31 -0500

    push to github

--- app.rb
+++ app.rb
@@ -12,8 +12,19 @@ get '/mull' do
   erb :input_form
 end
 
+get '/modal' do
+  erb :modal
+end
+
 post '/mull/' do
+  if params[:ramp].to_i < 0 || params[:ramp].to_i > 25
+    erb :error
+  elsif
+    params[:curve].to_i < 0 || params[:curve].to_i > 50
+    erb :error
+  else
   runner = Mulligan::VariableCountSim.new(1000, params[:ramp].to_i, params[:curve].to_i)
   @stats = runner.results
   erb :output
+  end
 end
